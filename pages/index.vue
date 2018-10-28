d483232d9e151e709e8ce9ea268a0d2d6ad6bcfc
naronA <otherthan.new@gmail.com>
2018-10-28 18:29:31 +0900

    ユーザーページ実装

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,12 @@
         <li v-for="item in items" :key="item.id">
           <h4>
             <span>{{item.title}}</span>
+            <small>
+              <span>by </span>
+              <nuxt-link :to="`/users/${item.user.id}`">
+                {{item.user.id}}
+              </nuxt-link>
+            </small>
           </h4>
           <div>{{item.body.slice(0,130)}}.....</div>
           <p><a :href="item.url">{{item.url}}</a></p>
@@ -16,15 +22,16 @@
 </template>
 
 <script>
-
 export default {
-  async asyncData({app}){
-    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+  async asyncData({ app }) {
+    const items = await app.$axios.$get(
+      'https://qiita.com/api/v2/items?query=tag:nuxt.js'
+    );
     return {
       items
-    }
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -38,7 +45,6 @@ h3 {
   padding: 8px 0;
   border-bottom: solid 1px #e5e5e5;
 }
-
 
 li + li {
   margin: 16px 0;
